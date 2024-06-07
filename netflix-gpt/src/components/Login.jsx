9becc47c4c3f3ab9d35adf9dfbf5ce9394c1a52d
vivekkeshtwal09@gmail.com <116489124+keshtwalvivek@users.noreply.github.com>
2024-06-07 12:42:22 +0530

    Login page

--- netflix-gpt/src/components/Login.jsx
+++ netflix-gpt/src/components/Login.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
 import Header from "./Header";
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState("true");
+
+  const handleSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div className="">
       <Header />
@@ -12,7 +19,19 @@ const Login = () => {
       </div>
       <div>
         <form className="w-3/12 absolute p-12 bg-black my-24 mx-auto right-0 left-0 bg-opacity-80 rounded-sm">
-          <h1 className="font-bold text-3xl py-4 text-white"> Sign In</h1>
+          <h1 className="font-bold text-3xl py-4 text-white">
+            {" "}
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </h1>
+
+          {!isSignIn && (
+            <input
+              type="text"
+              placeholder="username"
+              className="p-4 my-4 w-full bg-gray-700 rounded-sm"
+            />
+          )}
+
           <input
             type="email"
             placeholder="email"
@@ -23,10 +42,16 @@ const Login = () => {
             placeholder="password"
             className="p-4 my-4 w-full bg-gray-700 rounded-sm"
           />
-          <button className="p-4 my-6 bg-red-700 w-full rounded-sm">
+
+          <button className="p-4 my-6 bg-red-700 w-full rounded-sm cursor-pointer">
             {" "}
-            Sign In
+            {isSignIn ? "Sign In" : "Sign Up"}
           </button>
+          <p className="text-white cursor-pointer" onClick={handleSignIn}>
+            {isSignIn
+              ? "New to Netflix? Sign Up Mow"
+              : "Already regisered? Sign In Now"}
+          </p>
         </form>
       </div>
     </div>
